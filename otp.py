2d8b77cff408d40b0f8ef11a307ad210d52551c5
Piotr Zawislak <running.t@gmail.com>
2021-02-25 22:59:50 +0100

    secret file parameter may be path, or file, or file without extension - DONE

--- otp.py
+++ otp.py
@@ -7,7 +7,7 @@ import os
 def get_secret_path(secret: str):
     current_path = os.path.dirname(os.path.abspath(__file__))
     if os.pathsep in secret:  #assume its path
-        if os.path.isabs(secret) or secret.startswith('.'):
+        if os.path.isabs(secret) or not secret.startswith('.'):
             return secret
         else:
             return os.path.join(current_path, secret)
@@ -18,7 +18,7 @@ def get_secret_path(secret: str):
 
 
 def get_totp(secret_path: str):
-    with open(args.secret, 'r') as key:
+    with open(secret_path, 'r') as key:
         # for now secret is stored as a single line in file,
         # TODO: use .ini for example
         secret = key.readline().rstrip()
@@ -27,8 +27,8 @@ def get_totp(secret_path: str):
 
 def main(args):
     secret_path = get_secret_path(args.secret)
-    print(secret_path)
-    #print(get_totp(secret_path))
+    #print(secret_path)
+    print(get_totp(secret_path))
 
 
 if __name__ == '__main__':
