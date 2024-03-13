a1e9fb6b4ebfba02307ec909a8b31b78a0f88dc8
Piotr Zawislak <running.t@gmail.com>
2024-03-13 13:50:55 +0100

    feat: adding option to list all secrets

--- otp.py
+++ otp.py
@@ -4,6 +4,9 @@ import argparse
 import pyotp
 import os
 
+SECRETS_STORE = 'secrets'
+SECRET_EXT = '.secret'
+
 def get_secret_path(secret: str):
     current_path = os.path.dirname(os.path.abspath(__file__))
     if os.pathsep in secret:  #assume its path
@@ -12,10 +15,20 @@ def get_secret_path(secret: str):
         else:
             return os.path.join(current_path, secret)
     else:  #assume it
-        if not secret.endswith('.secret'):
-            secret+='.secret'
-        return os.path.join(current_path,'secrets', secret)
+        if not secret.endswith(SECRET_EXT):
+            secret+=SECRET_EXT
+        return os.path.join(current_path, SECRETS_STORE, secret)
 
+
+def get_secrets(path=SECRETS_STORE):
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(current_path, SECRETS_STORE)
+    return [os.path.splitext(f)[0] for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith(SECRET_EXT)]
+
+def list_secrets(secrets: list):
+    print("Secrets:")
+    for secret in secrets:
+        print(f"{secret}")
 
 def get_totp(secret_path: str):
     with open(secret_path, 'r') as key:
@@ -26,13 +39,17 @@ def get_totp(secret_path: str):
         return totp.now()
 
 def main(args):
-    secret_path = get_secret_path(args.secret)
-    #print(secret_path)
-    print(get_totp(secret_path))
+    if args.secret:
+        secret_path = get_secret_path(args.secret)
+        print(get_totp(secret_path))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Show (time based) OTP based on secret')
-    parser.add_argument('secret', type=str)
+    parser.add_argument('secret', nargs="?",  type=str)
+    parser.add_argument('-l', '--list', help="List stored secrets", action='store_true')
     args = parser.parse_args()
-    main(args)
+    if args.list:
+        list_secrets(get_secrets())
+    else:
+        main(args)
